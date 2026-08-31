4bbd97950ee85fd5b227cb88875215698d245650
JU893-2 <jongwon.um@lhsla.org>
2026-08-31 09:02:51 -0700

    assignment 1a is done

---
+++ assignment1a.py
@@ -0,0 +1,17 @@
+# Um, Jongwon 
+# Compute Programming, Period 4
+# Assignment: 1a
+# August 31 2026
+
+print("Hello World!")
+print("Hello my name is Jongwon!")
+print("I love python!")
+print("16")
+print(5+5)
+print(4*3)
+print(50-40)
+print(100/5)
+print(100//5)
+# when doing math with one /, we include the decimal point, when doing math with two /, we do not include the demcimal point
+print(2**4)
+print(6*3-4+8//2)
---
+++ assignment1b.py
@@ -0,0 +1,10 @@
+# Um, Jongwon
+# Computer Programming, period 4
+# Assignment: 1b
+# September 3 2026
+
+fav_hobby=("running")
+print(fav_hobby)
+x="andrew"
+
+
